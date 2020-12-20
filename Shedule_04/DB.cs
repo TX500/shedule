867869385c4f5f8c74f6a43aed89d9f8ffd2b7d3
Serg <mail@example.com>
2020-12-21 00:35:59 +0300

    Добавлен класс DB, настроена строка подключения (не проверена работа)

---
+++ Shedule_04/DB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shedule_04
+{
+    class DB
+    {
+        public static string connectString = @"Integrated Security=false; User Id = sa; 
+                                            Password = root; Initial Catalog = shedule; server = DESKTOP-80LGH2G\\SQLEXPRESS;";
+    }
+}
