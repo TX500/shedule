dcc38a120ba0eb1178d60ed7cc2ea7db1c510b7b
Serg <mail@example.com>
2020-12-21 00:29:55 +0300

    Певоначальная настройка MainForm

--- Shedule_04/Program.cs
+++ Shedule_04/Program.cs
@@ -16,7 +16,7 @@ namespace Shedule_04
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
