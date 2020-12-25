6eca5fbe3760d7fe7a82392c511d6c8f882a17cb
Serg <mail@example.com>
2020-12-25 21:03:18 +0300

    Преподаватели - Список

--- Shedule_04/LecturerForm.cs
+++ Shedule_04/LecturerForm.cs
@@ -7,14 +7,116 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Shedule_04
 {
     public partial class LecturerForm : Form
     {
+        Form modalLecturer = new ModalsForm.ModalLecturer();
+
         public LecturerForm()
         {
             InitializeComponent();
+        }
+
+        SqlConnection connect = new SqlConnection(DB.connectString);
+
+        private void tableLoad()
+        {
+            string querieAll = @"SELECT * FROM lecturer;";
+
+            SqlCommand table = new SqlCommand(querieAll, connect);
+
+            connect.Open();
+
+            SqlDataReader reader = table.ExecuteReader();
+
+            int i = 0;
+            int N = 1;
+            dataGridView1.Rows.Clear();
+
+            while (reader.Read())
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1[0, i].Value = reader[0];            // id_row
+                dataGridView1[1, i].Value = N;                    // №
+                string nameLecturer = reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString();
+                dataGridView1[2, i].Value = nameLecturer;
+                i++;
+                N++;
+            }
+            reader.Close();
+            connect.Close();
+        }
+
+        private void massDelete(string[] ids)
+        {
+            string id = "(";
+            for (int i = 0; i < ids.Length; i++)
+            {
+                if (i != ids.Length - 1)
+                {
+                    id += "'" + ids[i] + "',";
+                }
+                else
+                {
+                    id += "'" + ids[i] + "');";
+                }
+            }
+            string querieMassDelete = @"DELETE FROM faculty WHERE id_faculty in " + id;
+
+            if (MessageBox.Show("Вы действительно хотите выбранные записи? Данная операция необратима.", "Удаление", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    SqlCommand table = new SqlCommand(querieMassDelete, connect);
+
+                    connect.Open();
+                    SqlDataReader reader = table.ExecuteReader();
+                    reader.Close();
+                    connect.Close();
+                    tableLoad();
+                }
+                catch (SqlException ex)
+                {
+                    connect.Close();
+                    MessageBox.Show(ex.Number.ToString(), "Неизвестная ошибка.");
+                }
+            }
+        }
+
+        private void addLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ModalsForm.ModalLecturer.isNewItem = true;
+            modalLecturer.ShowDialog();
+
+        }
+
+        private void reloadLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            tableLoad();
+        }
+
+        private void deleteLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Получаем ИД записей и записываем их в массив ids
+            string[] ids = new string[dataGridView1.SelectedRows.Count];
+
+            for (int i = 0; i < dataGridView1.SelectedRows.Count; i++)
+            {
+                int row = dataGridView1.SelectedRows[i].Index;
+                ids[i] = dataGridView1[0, row].Value.ToString();
+            }
+            massDelete(ids);
+        }
+
+        private void dataGridView2_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = dataGridView1.CurrentRow.Index;
+            ModalsForm.ModalLecturer.idItem = dataGridView1[0, row].Value.ToString();
+            ModalsForm.ModalLecturer.isNewItem = false;
+            modalLecturer.ShowDialog();
         }
     }
 }
---
+++ Shedule_04/ModalsForm/ModalLecturer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Shedule_04.ModalsForm
+{
+    public partial class ModalLecturer : Form
+    {
+
+        public static bool isNewItem;
+
+        public static string idItem;
+
+        string oldName;
+
+        public ModalLecturer()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connect = new SqlConnection(DB.connectString);
+
+    }
+}
