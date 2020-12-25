7c20dedcf3756bf51bd6a8422b9e336626e782ab
Serg <mail@example.com>
2020-12-25 23:05:07 +0300

    Доделаны Преподаватели. Не тестировано

--- Shedule_04/ModalsForm/ModalLecturer.Designer.cs
+++ Shedule_04/ModalsForm/ModalLecturer.Designer.cs
@@ -28,12 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cancel = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxMiddlename = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cancel
+            // 
+            this.cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancel.Location = new System.Drawing.Point(276, 257);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(118, 34);
+            this.cancel.TabIndex = 11;
+            this.cancel.Text = "Отмена";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // save
+            // 
+            this.save.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.save.Location = new System.Drawing.Point(133, 257);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(118, 34);
+            this.save.TabIndex = 10;
+            this.save.Text = "Сохранить";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSurname.Location = new System.Drawing.Point(66, 74);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(408, 26);
+            this.textBoxSurname.TabIndex = 9;
+            this.textBoxSurname.TextChanged += new System.EventHandler(this.textBoxSurname_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(194, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Преподаватель";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(62, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(62, 113);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Имя";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxName.Location = new System.Drawing.Point(66, 136);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(408, 26);
+            this.textBoxName.TabIndex = 13;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // textBoxMiddlename
+            // 
+            this.textBoxMiddlename.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMiddlename.Location = new System.Drawing.Point(66, 200);
+            this.textBoxMiddlename.Name = "textBoxMiddlename";
+            this.textBoxMiddlename.Size = new System.Drawing.Size(408, 26);
+            this.textBoxMiddlename.TabIndex = 15;
+            this.textBoxMiddlename.TextChanged += new System.EventHandler(this.textBoxMiddlename_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(62, 177);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Отчество";
+            // 
+            // ModalLecturer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(528, 319);
+            this.ControlBox = false;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxMiddlename);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ModalLecturer";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Text = "ModalLecturer";
+            this.Load += new System.EventHandler(this.ModalLecturer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxMiddlename;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Shedule_04/ModalsForm/ModalLecturer.cs
+++ Shedule_04/ModalsForm/ModalLecturer.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace Shedule_04.ModalsForm
 {
@@ -27,5 +20,168 @@ namespace Shedule_04.ModalsForm
 
         SqlConnection connect = new SqlConnection(DB.connectString);
 
+        private void ModalLecturer_Load(object sender, EventArgs e)
+        {
+            if (isNewItem == false)
+            {
+                // Получаем значение для поля по ИД
+                try
+                {
+                    string updateItemName = @"SELECT surname, name, middle_name FROM lecturer WHERE id_lecturer = " + idItem + ";";
+                    SqlCommand table = new SqlCommand(updateItemName, connect);
+                    connect.Open();
+                    SqlDataReader reader = table.ExecuteReader();
+                    reader.Read();
+                    textBoxSurname.Text = reader[0].ToString();
+                    textBoxName.Text = reader[1].ToString();
+                    textBoxMiddlename.Text = reader[2].ToString();
+                    oldName = textBoxSurname.Text + textBoxName.Text + textBoxMiddlename.Text;
+                    connect.Close();
+                }
+                catch (SqlException ex)
+                {
+                    connect.Close();
+                    MessageBox.Show(ex.Number.ToString(), "Неизвестная ошибка.");
+                }
+            }
+            else
+            {
+                textBoxSurname.Text = textBoxName.Text = textBoxMiddlename.Text = oldName = "";
+            }
+        }
+
+        private void textBoxSurname_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxSurname.Text.Length == 1 && textBoxSurname.Text.Contains(" "))
+            {
+                textBoxSurname.Text = "";
+            }
+            if (textBoxSurname.Text.IndexOf(" ") == 0)
+            {
+                textBoxSurname.Text = textBoxSurname.Text.Substring(1);
+                textBoxSurname.SelectionStart = textBoxSurname.Text.Length;
+            }
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxName.Text.Length == 1 && textBoxName.Text.Contains(" "))
+            {
+                textBoxName.Text = "";
+            }
+            if (textBoxName.Text.IndexOf(" ") == 0)
+            {
+                textBoxName.Text = textBoxName.Text.Substring(1);
+                textBoxName.SelectionStart = textBoxName.Text.Length;
+            }
+        }
+
+        private void textBoxMiddlename_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxMiddlename.Text.Length == 1 && textBoxMiddlename.Text.Contains(" "))
+            {
+                textBoxMiddlename.Text = "";
+            }
+            if (textBoxMiddlename.Text.IndexOf(" ") == 0)
+            {
+                textBoxMiddlename.Text = textBoxMiddlename.Text.Substring(1);
+                textBoxMiddlename.SelectionStart = textBoxMiddlename.Text.Length;
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (textBoxSurname.Text == "" || textBoxName.Text == "")
+            {
+                MessageBox.Show("Заполните обязательные поля");
+            }
+            else
+            {
+                while (textBoxName.Text[textBoxName.Text.Length - 1] == ' ')
+                {
+                    textBoxSurname.Text = textBoxSurname.Text.Substring(0, textBoxSurname.Text.Length - 1);
+                }
+                while (textBoxName.Text[textBoxName.Text.Length - 1] == ' ')
+                {
+                    textBoxName.Text = textBoxName.Text.Substring(0, textBoxName.Text.Length - 1);
+                }
+                while (textBoxMiddlename.Text[textBoxMiddlename.Text.Length - 1] == ' ')
+                {
+                    textBoxMiddlename.Text = textBoxMiddlename.Text.Substring(0, textBoxMiddlename.Text.Length - 1);
+                }
+            }
+
+            if (isNewItem == true && textBoxSurname.Text != "" && textBoxName.Text != "")
+            {
+                try
+                {
+                    string querieAdd = @"INSERT INTO lecturer(surname, name, middle_name) VALUES('" + textBoxSurname.Text + "', '" + textBoxName.Text + "', '" + textBoxMiddlename.Text + "');";
+                    SqlCommand insert = new SqlCommand(querieAdd, connect);
+                    connect.Open();
+                    insert.ExecuteNonQuery();
+                    connect.Close();
+                    textBoxSurname.Text = "";
+                    textBoxName.Text = "";
+                    textBoxMiddlename.Text = "";
+                    this.Close();
+
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Number.ToString(), "Неизвестная ошибка.");
+                }
+            }
+
+            if (isNewItem == false && oldName != (textBoxSurname.Text + textBoxName.Text + textBoxMiddlename.Text))
+            {
+                if (MessageBox.Show("Вы действительно хотите внести изменения? Данная операция необратима.", "Изменение", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        string queiryUpdate = @"UPDATE lecturer SET surname='" + textBoxSurname.Text + "', name='" + textBoxName.Text + "', middle_name='" + textBoxMiddlename.Text + "' WHERE id_lecturer ='" + idItem + "'";
+                        SqlCommand update = new SqlCommand(queiryUpdate, connect);
+                        connect.Open();
+                        update.ExecuteNonQuery();
+                        connect.Close();
+                        this.Close();
+                    }
+                    catch (SqlException ex)
+                    {
+                        connect.Close();
+                        MessageBox.Show(ex.Number.ToString(), "Неизвестная ошибка.");
+                    }
+                }
+            }
+
+            if (isNewItem == false && oldName == textBoxName.Text)
+            {
+                this.Close();
+            }
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            if (textBoxSurname.Text.Length == 0 && textBoxName.Text.Length == 0 && isNewItem == true)
+            {
+                this.Close();
+                isNewItem = false;
+            }
+            else
+            {
+                if (oldName != textBoxSurname.Text + textBoxName.Text + textBoxMiddlename.Text)
+                {
+                    if (MessageBox.Show("Вы действительно хотите закрыть окно? Изменения не будут применены", "Отмена", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        textBoxSurname.Text = textBoxName.Text = textBoxMiddlename.Text = "";
+                        this.Close();
+                        isNewItem = false;
+                    }
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
     }
 }
