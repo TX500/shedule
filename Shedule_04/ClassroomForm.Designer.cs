c16fa4eea52a7f8c5d5b46b17fd5af3e5fc5e8db
Serg <mail@example.com>
2021-01-06 01:07:29 +0300

    Настройка ГридВью часть 2

--- Shedule_04/ClassroomForm.Designer.cs
+++ Shedule_04/ClassroomForm.Designer.cs
@@ -111,6 +111,7 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -121,6 +122,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(800, 405);
             this.dataGridView1.TabIndex = 2;
             // 
